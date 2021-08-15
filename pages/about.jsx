2b9d8b8478d143b024c9f4e6797eefb9ebe27b43
Ariful Islam Khan <61895871+andrenoari@users.noreply.github.com>
2021-08-15 14:42:16 +0530

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -30,47 +30,6 @@ const AboutPage = () => {
         </Center>
       </Container>
 
-      {/* //partners */}
-      <Container bg="#FFFFFF" maxW="container.xl" px={{base: '6', '2xl': '0'}} mb="8">
-        <Center>
-          <Heading as="h2" size="2xl" pt="3rem" pb="3rem">
-            Our Partners
-          </Heading>
-        </Center>
-        <Box px="5" pb="10">
-          <SimpleGrid columns={[1, null, 2]}>
-            <Center>
-              <Image src="partners/pfolk1--.png" w="50%" />
-            </Center>
-            <Center>
-              <Image src="partners/navgurukul--.png" w="50%" />
-            </Center>
-          </SimpleGrid>
-        </Box>
-      </Container>
-
-      {/* //collaborators */}
-      <Container bg="#FAFAFA" maxW="container.xl" px={{base: '6', '2xl': '0'}} mb="8">
-        <Center>
-          <Heading as="h2" size="2xl" p="3rem">
-            Our Team
-          </Heading>
-        </Center>
-        <SimpleGrid columns={[1, 2, 3]} spacing="30px">
-          {TeamData.map((member, index) => {
-            return (
-              <Profile
-                name={member.name}
-                profileLink={member.profileLink}
-                imageLink={member.imageLink}
-                socialType={member.socialType}
-                key={index}
-              />
-            )
-          })}
-        </SimpleGrid>
-      </Container>
-
       <Footer />
     </>
   )
